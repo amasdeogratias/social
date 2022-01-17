6e0b0ab3918c89daf2267620e2c2c35d2c5fbc64
amasdeogratias <deogratiasprog@gmail.com>
2022-01-17 15:44:58 +0300

    adding profile options

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,5 +1,23 @@
+from dataclasses import fields
+from pyexpat import model
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import User, Group
+from .models import Profile
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+    
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+    inlines = [ProfileInline]
+     
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+# admin.site.register(Profile)
+        
